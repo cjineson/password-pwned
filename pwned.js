dd5d1afd61b5be0eef03651f66f8890775d754cd
Chris Ineson <chrisineson@Chriss-MBP.lan>
2019-05-11 20:37:49 +0100

    Update readme

--- pwned.js
+++ pwned.js
@@ -1,20 +1,3 @@
-// Check SHA1 Hash of password value against known 
-// compromised passwords list from HaveIBeenPwned.com
-// First 5 chars of SHA1 sent as querystring & 
-// receive CRLF-delimited list of SHA1 suffix & count
-//
-// e.g. using 'password123':
-// Generated SHA1: cbfdac6008f9cab4083784cbd1874f76618d2a97
-// GET https://api.pwnedpasswords.com/range/cbfda
-// Pwned SHA Suffixes: 
-// 00791BB54CC9122C70C1156FD97134EB83E:3
-// 008CDEBE10E31BF09C9BD20CBCC2C9CEDA3:2
-// ...
-// c6008f9cab4083784cbd1874f76618d2a97:116847
-// Looking for SHA1 Suffix: C6008F9CAB4083784CBD1874F76618D2A97
-// Password pwned 116847 times
-//
-// 11/05/19 Chris Ineson <chrisineson@gmail.com>
 const hostname = 'api.pwnedpasswords.com'
 const debug = false;
 
